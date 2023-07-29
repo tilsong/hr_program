3744eeb5db3e643c7948887bad8f22de2f96491c
tilsong <cnsong0229@gmail.com>
2023-07-29 14:44:33 +0900

    [#1] 프로젝트 의존성 설정

---
+++ src/main/java/com/example/hr_program/HrProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hr_program/HrProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hr_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
